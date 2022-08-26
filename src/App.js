91e6eaf7fe6331782daf11bfbe0108254e39e428
imstar15 <imstar15@qq.com>
2022-08-27 01:27:12 +0800

    Read price from chain

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
 import './App.css';
 import { Row, Col } from 'antd';
+import { useEffect } from 'react';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+
+
+const OAK_WS_ENDPOINT = 'wss://rpc.turing-staging.oak.tech'
 
 function App() {
+  const getPolkadotApi = async () => {
+    const wsProvider = new WsProvider(OAK_WS_ENDPOINT);
+    const polkadotApi = await ApiPromise.create({
+      provider: wsProvider,
+      rpc: {
+        automationTime: {
+          generateTaskId: {
+            description: 'Getting task ID given account ID and provided ID',
+            params: [
+              {
+                name: 'accountId',
+                type: 'AccountId',
+              },
+              {
+                name: 'providedId',
+                type: 'Text',
+              },
+            ],
+            type: 'Hash',
+          },
+        },
+      },
+    });
+    return polkadotApi;
+  }
+
+  useEffect( () => {
+    const getPrice = async () => {
+      const polkadotApi = await getPolkadotApi();
+      const price = await polkadotApi.query.automationPrice.assetPrices('mgx:ksm');
+      console.log('price: ', price.toString());
+    }
+    getPrice();
+  }, []);
+
   return (
     <div className='container page-container'>
       <Row>
